b10103b3f5f513780a1e5e6865b12da1cbf78b20
Pirjakun <90229340+Pirjakun@users.noreply.github.com>
2025-10-10 12:57:27 +0700

    Update AmbilMK.java

--- SMST 5/PBO/TP4/Belajar/Belajar4/src/main/java/com/mycompany/belajar4/AmbilMK.java
+++ SMST 5/PBO/TP4/Belajar/Belajar4/src/main/java/com/mycompany/belajar4/AmbilMK.java
@@ -1,41 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.belajar4;
-
-/**
- *
- * @author Fyrza
- */
 public class AmbilMK {
-    private MataKuliah mk;
-    private String nilaiHuruf;
-    private String thnAjar;
+    private MataKuliah mataKuliah;
+    private String nilai;
+    private String tahunAjaran;
 
-    public AmbilMK(MataKuliah mk, String nilaiHuruf, String thnAjar) {
-        this.mk = mk;
-        this.nilaiHuruf = nilaiHuruf;
-        this.thnAjar = thnAjar;
+    public AmbilMK(MataKuliah mk, String nilai, String tahunAjaran) {
+        this.mataKuliah = mk;
+        this.nilai = nilai;
+        this.tahunAjaran = tahunAjaran;
     }
 
-    public MataKuliah getMK() {
-        return mk;
-    }
+    public MataKuliah getMataKuliah() { return mataKuliah; }
+    public void setMataKuliah(MataKuliah mk) { this.mataKuliah = mk; }
 
-    public String getThnAjar() {
-        return thnAjar;
-    }
+    public String getNilai() { return nilai; }
+    public void setNilai(String nilai) { this.nilai = nilai; }
 
-    public double nilaiAngka() {
-        switch (nilaiHuruf) {
-            case "A":  return 4.0;
-            case "AB": return 3.5;
-            case "B":  return 3.0;
-            case "BC": return 2.5;
-            case "C":  return 2.0;
-            case "D":  return 1.0;
-            default:   return 0.0;
-        }
-    }
+    public String getTahunAjaran() { return tahunAjaran; }
+    public void setTahunAjaran(String tahunAjaran) { this.tahunAjaran = tahunAjaran; }
 }

03be10c00a8dcd5d8184f23c0222d4ff0fe6be67
Pirjakun <90229340+Pirjakun@users.noreply.github.com>
2025-10-10 12:55:54 +0700

    Update Main.java

--- SMST 5/PBO/TP4/TP4/src/main/java/com/mycompany/tp4/Main.java
+++ SMST 5/PBO/TP4/TP4/src/main/java/com/mycompany/tp4/Main.java
@@ -1,39 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+import java.util.Scanner;
 
-package com.mycompany.tp4;
-
-/**
- *
- * @author Fyrza
- */
 public class Main {
-
     public static void main(String[] args) {
-        Dokter dt1 = new Dokter("Grisha", "Mata");
-        Dokter dt2 = new Dokter("Faye", "Gigi");
-        Dokter dt3 = new Dokter("Zeke", "Telinga");
-        
-        Pasien ps1 = new Pasien("historia", 20);
-        Pasien ps2 = new Pasien("Sasha", 37);
-        Pasien ps3 = new Pasien("Petra", 55);
-        
-        Pemesanan[] arr = new Pemesanan[5];
-        arr[0] = new Pemesanan(dt1, ps1, "Selasa");
-        arr[1] = new Pemesanan(dt2, ps1, "Kamis");
-        arr[2] = new Pemesanan(dt2, ps2, "Selasa");
-        arr[3] = new Pemesanan(dt3, ps2, "Jumat");
-        arr[4] = new Pemesanan(dt1, ps3, "Senin");
-        
-        for (int i=0; i < arr.length; i++){
-            Pemesanan p = arr[i];
-            System.out.println("Urutan ke-"+(i+1));
-            System.out.println("Nama Dokter     : "+ p.getDokter().getNama());
-            System.out.println("Spesialis       : "+ p.getDokter().getSpesialis());
-            System.out.println("Nama Pasien     : "+ p.getPasien().getNama() + " (" + p.getPasien().getUmur() + ")");
-            System.out.println("Hari            : "+p.getHari());
-            System.out.println("");
+        MataKuliah pbo = new MataKuliah("CII3B4", "Pemrograman Berorientasi Objek", 4);
+        MataKuliah kalkulus = new MataKuliah("CII2A3", "Kalkulus", 3);
+        MataKuliah dka = new MataKuliah("CII1F4", "Dasar Kecerdasan Artifisial", 4);
+        MataKuliah kwu = new MataKuliah("UKI2A2", "Kewirausahaan", 2);
+
+        Mahasiswa mhs1 = new Mahasiswa("103012330004", "Tanjiro", 1);
+        Mahasiswa mhs2 = new Mahasiswa("103012310024", "Spongebob", 12);
+        Mahasiswa mhs3 = new Mahasiswa("103012320012", "Naruto", 5);
+
+        mhs1.setKelas("47", 1);
+        mhs2.setKelas("47", 12);
+        mhs3.setKelas("47", 5);
+
+        mhs1.tambahMK(new AmbilMK(pbo, "A", "21/22"));
+        mhs1.tambahMK(new AmbilMK(kalkulus, "A", "09/10"));
+        mhs1.tambahMK(new AmbilMK(dka, "A", "21/22"));
+        mhs1.tambahMK(new AmbilMK(kwu, "A", "21/22"));
+
+        mhs2.tambahMK(new AmbilMK(pbo, "A", "21/22"));
+        mhs2.tambahMK(new AmbilMK(kalkulus, "A", "09/10"));
+        mhs2.tambahMK(new AmbilMK(dka, "A", "09/10"));
+        mhs2.tambahMK(new AmbilMK(kwu, "A", "21/22"));
+        mhs2.tambahMK(new AmbilMK(pbo, "B", "21/22"));
+        mhs2.tambahMK(new AmbilMK(kalkulus, "A", "19/20"));
+        mhs2.tambahMK(new AmbilMK(dka, "BC", "19/20"));
+        mhs2.tambahMK(new AmbilMK(kwu, "AB", "21/22"));
+
+        mhs3.tambahMK(new AmbilMK(pbo, "AB", "22/23"));
+        mhs3.tambahMK(new AmbilMK(kalkulus, "D", "21/22"));
+        mhs3.tambahMK(new AmbilMK(dka, "B", "21/22"));
+        mhs3.tambahMK(new AmbilMK(kwu, "C", "22/23"));
+
+        Mahasiswa[] daftar = new Mahasiswa[] { mhs1, mhs2, mhs3 };
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Masukkan Tahun Ajaran (ketik q untuk exit program): ");
+            String input = sc.nextLine().trim();
+            if (input.equalsIgnoreCase("q")) break;
+            boolean any = false;
+            for (Mahasiswa m : daftar) {
+                double ipk = m.hitungIPK(input);
+                if (ipk >= 0.0) {
+                    any = true;
+                    System.out.printf("IPK %s (%s): %.2f%n", m.getNama(), m.getKelas(), ipk);
+                }
+            }
+            if (!any) {
+                System.out.println("Tidak ada histori nilai pada Tahun Ajaran ini");
+            }
+            System.out.println();
         }
+        System.out.println("Program selesai. Semoga selalu diberi kemudahan^^");
+        sc.close();
     }
 }

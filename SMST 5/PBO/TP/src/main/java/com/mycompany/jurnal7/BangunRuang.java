f5eddae87ebeaa3e292882d07c3d58f04e8ccf11
RaVan4 <90229340+Pirjakun@users.noreply.github.com>
2025-10-31 12:16:32 +0700

    Remove license comments from BangunRuang interface

--- SMST 5/PBO/TP/src/main/java/com/mycompany/jurnal7/BangunRuang.java
+++ SMST 5/PBO/TP/src/main/java/com/mycompany/jurnal7/BangunRuang.java
@@ -1,14 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.jurnal7;
-
-/**
- *
- * @author Fyrza
- */
 public interface BangunRuang {
     double hitungLuasPermukaan();
     double hitungVolume();
+
 }

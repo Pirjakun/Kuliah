0f669600b68e0b2752d3060daf007e1e73e636ff
RaVan4 <90229340+Pirjakun@users.noreply.github.com>
2025-10-31 12:17:11 +0700

    Remove unnecessary comments from Kubus.java

--- SMST 5/PBO/TP/src/main/java/com/mycompany/jurnal7/Kubus.java
+++ SMST 5/PBO/TP/src/main/java/com/mycompany/jurnal7/Kubus.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.jurnal7;
-
-/**
- *
- * @author Fyrza
- */
 public class Kubus extends Bentuk implements BangunRuang {
     private int sisi;
 
@@ -31,4 +21,5 @@ public class Kubus extends Bentuk implements BangunRuang {
     public double hitungVolume() {
         return sisi * sisi * sisi;
     }
+
 }
